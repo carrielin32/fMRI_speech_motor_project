3388de6dc53d25a312caa5ae62c53c4e87eaeb64
Carrielin <30627002+carrielin32@users.noreply.github.com>
2019-04-29 11:05:19 +0200

    Update fMRI_Preprocessig_speech_motor.m

--- fMRI_Preprocessig_speech_motor.m
+++ fMRI_Preprocessig_speech_motor.m
@@ -67,7 +67,7 @@ function DoSliceTiming(w)
     matlabbatch{1}.spm.temporal.st.tr = w.TR;
     matlabbatch{1}.spm.temporal.st.ta = 1.94444444444444; 
     matlabbatch{1}.spm.temporal.st.so = [2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35]; 
-    % consult the slice order again? 待改
+    % no problem here 
     matlabbatch{1}.spm.temporal.st.refslice = 36;      
     matlabbatch{1}.spm.temporal.st.prefix = 'a'; 
     
