de86c2284ad42a697786f1f52026581ba87bad2b
Carrielin <30627002+carrielin32@users.noreply.github.com>
2019-06-05 10:37:43 +0200

    Update fMRI_assignment_part2.m

--- fMRI_assignment_part2.m
+++ fMRI_assignment_part2.m
@@ -13,7 +13,9 @@ spm_jobman('initcfg');
 
 %%======define parameters in a general structure 'w'=====
 w.dataDir  = '/Users/carrielin/Documents/MATLAB/speech_motor_project/1_Data/';  %raw data
-w.subjects = {'2','3', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16','17','19','20'}; % without low accuracy ones (parent= dataDir)
+%w.subjects = {'2','3', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16','17','19','20'}; % without low accuracy ones (parent= dataDir)
+%w.subjects = {'03','04', '05', '06', '07', '08', '09','10', '11', '12', '13', '14', '15', '16','17','18','19','20'};
+w.subjects ={'04'}; % test
 
 w.structDir = 't1'; % structural directory (parent=subject)
 w.sessions = {'run1','run2','run3','run4'}; %session directory (parent=subject)
@@ -60,7 +62,7 @@ function DoFirstLevel(w)
 
     %session 1 -- syllable 
     %matlabbatch{1}.spm.stats.fmri_spec.sess(1).scans = cellstr(spm_select('ExtFPList',  fullfile(w.subPath, 'picnam', '^swua','.*\.nii$', Inf)));
-    EPI_1 = spm_select('ExtFPList',  fullfile(w.subPath,'run1'), ['^swua' '.*' '.*\.nii$'], Inf);
+    EPI_1 = spm_select('ExtFPList',  fullfile(w.subPath,'run1'), '^swua.*\.nii$', Inf);
     
     matlabbatch{1}.spm.stats.fmri_spec.sess(1).scans = cellstr(EPI_1);
    
@@ -82,7 +84,7 @@ function DoFirstLevel(w)
    % matlabbatch{1}.spm.stats.fmri_spec.sess(1).cond(1).orth = 1;
     
     % 0503 add 
-    con1=spm_select('FPList', fullfile(w.dataDir, 'onsets', 'tone', w.subName,['^Run1' '.*\.mat$'];
+    con1=spm_select('FPList', fullfile(w.dataDir, 'onsets', 'tone', w.subName,'^Run1.*\.mat$'))
     matlabbatch{1}.spm.stats.fmri_spec.sess(1).multi = cellstr(con1);
     
     matlabbatch{1}.spm.stats.fmri_spec.sess(1).regress = struct('name', {}, 'val', {});
@@ -92,7 +94,7 @@ function DoFirstLevel(w)
 
     %session 2 -- tone
     %matlabbatch{1}.spm.stats.fmri_spec.sess(2).scans = cellstr(spm_select('ExtFPList',  fullfile(w.subPath, 'verbgen', '^swua','.*\.nii$', Inf)));
-    EPI_2 = spm_select('ExtFPList',  fullfile(w.subPath,'run2'), ['^swua' '.*' '.*\.nii$'], Inf);
+    EPI_2 = spm_select('ExtFPList',  fullfile(w.subPath,'run2'), '^swua.*\.nii$', Inf);
     
     matlabbatch{1}.spm.stats.fmri_spec.sess(2).scans = cellstr(EPI_2);
     
@@ -112,41 +114,41 @@ function DoFirstLevel(w)
    % matlabbatch{1}.spm.stats.fmri_spec.sess(2).cond(1).pmod = struct('name', {}, 'param', {}, 'poly', {});
    % matlabbatch{1}.spm.stats.fmri_spec.sess(2).cond(1).orth = 1;
     
-    con2=spm_select('FPList', fullfile(w.dataDir, 'onsets', 'tone', w.subName,['^Run2' '.*\.mat$'];
+    con2=spm_select('FPList', fullfile(w.dataDir, 'onsets', 'tone', w.subName,'^Run2.*\.mat$'))
     matlabbatch{1}.spm.stats.fmri_spec.sess(2).multi = cellstr(con2);
    
     matlabbatch{1}.spm.stats.fmri_spec.sess(2).regress = struct('name', {}, 'val', {});
-    rpF2=spm_select('FPList', fullfile(w.subPath, 'run2'),['^rp' '.*\.txt$']);
+    rpF2=spm_select('FPList', fullfile(w.subPath, 'run2'),'^rp*\.txt$');
     matlabbatch{1}.spm.stats.fmri_spec.sess(2).multi_reg = cellstr(rpF2);
     matlabbatch{1}.spm.stats.fmri_spec.sess(2).hpf = Inf;
     
     %session3 --syllable
-    EPI_3 = spm_select('ExtFPList',  fullfile(w.subPath,'run3'), ['^swua' '.*' '.*\.nii$'], Inf);
+    EPI_3 = spm_select('ExtFPList',  fullfile(w.subPath,'run3'), '^swua.*\.nii$', Inf);
     
     matlabbatch{1}.spm.stats.fmri_spec.sess(3).scans = cellstr(EPI_3);
     
     matlabbatch{1}.spm.stats.fmri_spec.sess(3).cond = struct('name', {}, 'onset', {}, 'duration', {}, 'tmod', {}, 'pmod', {});
     
-    con3=spm_select('FPList', fullfile(w.dataDir, 'onsets', 'tone', w.subName,['^Run3' '.*\.mat$'];
+    con3=spm_select('FPList', fullfile(w.dataDir, 'onsets', 'tone', w.subName,'^Run3.*\.mat$'))
     matlabbatch{1}.spm.stats.fmri_spec.sess(3).multi = cellstr(con3);
    
     matlabbatch{1}.spm.stats.fmri_spec.sess(3).regress = struct('name', {}, 'val', {});
-    rpF3=spm_select('FPList', fullfile(w.subPath, 'run3'),['^rp' '.*\.txt$']);
+    rpF3=spm_select('FPList', fullfile(w.subPath, 'run3'),'^rp.*\.txt$');
     matlabbatch{1}.spm.stats.fmri_spec.sess(3).multi_reg = cellstr(rpF3);
     matlabbatch{1}.spm.stats.fmri_spec.sess(3).hpf = Inf;
     
      %session4 --tone
-    EPI_4 = spm_select('ExtFPList',  fullfile(w.subPath,'run4'), ['^swua' '.*' '.*\.nii$'], Inf);
+    EPI_4 = spm_select('ExtFPList',  fullfile(w.subPath,'run4'), '^swua.*\.nii$', Inf);
     
     matlabbatch{1}.spm.stats.fmri_spec.sess(4).scans = cellstr(EPI_4);
     
     matlabbatch{1}.spm.stats.fmri_spec.sess(4).cond = struct('name', {}, 'onset', {}, 'duration', {}, 'tmod', {}, 'pmod', {});
     
-    con4=spm_select('FPList', fullfile(w.dataDir, 'onsets', 'tone', w.subName,['^Run4' '.*\.mat$'];
+    con4=spm_select('FPList', fullfile(w.dataDir, 'onsets', 'tone', w.subName,'^Run4.*\.mat$'))
     matlabbatch{1}.spm.stats.fmri_spec.sess(4).multi = cellstr(con4);
    
     matlabbatch{1}.spm.stats.fmri_spec.sess(4).regress = struct('name', {}, 'val', {});
-    rpF4=spm_select('FPList', fullfile(w.subPath, 'run4'),['^rp' '.*\.txt$']);
+    rpF4=spm_select('FPList', fullfile(w.subPath, 'run4'),'^rp.*\.txt$');
     matlabbatch{1}.spm.stats.fmri_spec.sess(4).multi_reg = cellstr(rpF4);
     matlabbatch{1}.spm.stats.fmri_spec.sess(4).hpf = Inf;
     
